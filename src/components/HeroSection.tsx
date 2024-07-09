474f9ee5e7a430ef7f5a2039ad74b7093af75d84
seolyam <leeyam.dev@gmail.com>
2024-07-09 10:54:41 +0800

    added random meal suggestion

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import useSWR from "swr";
 
 interface Meal {
@@ -24,20 +25,26 @@ const HeroSection: React.FC = () => {
   const meal: Meal = data.meals[0];
 
   return (
-    <div className="font-sans flex flex-col items-center bg-gray-100 p-6 rounded-lg shadow-lg max-w-4xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">A meal you might like</h1>
-      <img
-        src={meal.strMealThumb}
-        alt={meal.strMeal}
-        className="w-full h-64 object-cover rounded-lg shadow-lg mb-4"
-      />
-      <h2 className="text-3xl font-bold mt-2">{meal.strMeal}</h2>
-      <p className="mt-2 text-lg text-gray-700">
-        {meal.strCategory} | {meal.strArea}
-      </p>
-      <p className="mt-4 text-gray-600 text-center">
-        {meal.strInstructions.substring(0, 150)}...
-      </p>
+    <div className="font-sans flex flex-row items-center bg-gray-100 p-6 rounded-xl shadow-lg max-w-4xl mx-auto gap-2">
+      <div className="flex-col flex-2 grow">
+        <h1 className="text-2xl font-bold mb-4">A meal you might like:</h1>
+        <Link to={`/meal/${meal.idMeal}`} key={meal.idMeal}>
+          <img
+            src={meal.strMealThumb}
+            alt={meal.strMeal}
+            className="w-full h-64 object-cover rounded shadow-lg mb-4 transition-transform duration-200 ease-in-out transform hover:scale-105 "
+          />
+          <h2 className="text-3xl font-bold mt-2">{meal.strMeal}</h2>
+          <p className=" text-lg text-gray-700">
+            {meal.strCategory} | {meal.strArea}
+          </p>
+        </Link>
+      </div>
+      <div className="flex-wrap w-[40%]">
+        <p className="mt-4 text-gray-600 text-center flex-1 text-lg">
+          {meal.strInstructions.substring(0, 150)}...
+        </p>
+      </div>
     </div>
   );
 };
