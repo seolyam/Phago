9c7efc83e744000c949bda1a28bb2bfb892934fa
seolyam <leeyam.dev@gmail.com>
2024-07-12 12:07:33 +0800

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="max-w-6xl mx-auto px-4 text-center">
+        <p>&copy; 2024 Phago. All rights reserved.</p>
+        <div className="flex justify-center space-x-4 mt-4">
+          <a href="/about" className="hover:underline">
+            About
+          </a>
+          <a href="/contact" className="hover:underline">
+            Contact
+          </a>
+          <a href="/privacy" className="hover:underline">
+            Privacy Policy
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
