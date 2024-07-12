a7d5c92b5ab0a72093eb8ec92969d0375a6fd24d
seolyam <leeyam.dev@gmail.com>
2024-07-12 12:03:28 +0800

    feat: contact

---
+++ src/components/Contact.tsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import Image from "../img/profile.jpg";
+
+const Contact = () => {
+  return (
+    <div className="flex flex-col ml-10 items-start p-8 min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Contact Me</h1>
+      <img
+        src={Image}
+        alt="Profile"
+        className="w-48 h-48 rounded-full mb-8 object-cover shadow-lg"
+      />
+      <div className="text-lg space-y-4">
+        <div>
+          <Link
+            to="https://github.com/seolyam"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black hover:underline"
+          >
+            GitHub
+          </Link>
+        </div>
+        <div>
+          <a
+            href="mailto:leeyam.dev@gmail.com"
+            className="text-black hover:underline"
+          >
+            leeyam.dev@gmail.com
+          </a>
+        </div>
+        <div>
+          <Link
+            to="https://www.linkedin.com/in/lhiam-andrei-lingco-5b1b622a0/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black hover:underline"
+          >
+            LinkedIn
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
