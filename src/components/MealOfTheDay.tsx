0b1ccef35115388713b86126e71c6ca61516f1f9
seolyam <leeyam.dev@gmail.com>
2024-07-27 13:21:04 +0800

    chore: fix MealOfTheDay not storing meal correctly

--- src/components/MealOfTheDay.tsx
+++ src/components/MealOfTheDay.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
-import useSWR from "swr";
 import { Meal } from "../types/Meal";
 import { getStoredMeal, storeMeal, isMealExpired } from "../utils";
 import { Link } from "react-router-dom";
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const MealOfTheDay = () => {
   const [meal, setMeal] = useState<Meal | null>(null);
@@ -14,29 +11,22 @@ const MealOfTheDay = () => {
 
     if (storedMealData && !isMealExpired(storedMealData.timestamp)) {
       setMeal(storedMealData.meal);
+    } else {
+      fetchNewMeal();
     }
   }, []);
 
-  const { data, error } = useSWR(
-    meal ? null : "https://www.themealdb.com/api/json/v1/1/random.php",
-    fetcher
-  );
-
-  useEffect(() => {
+  const fetchNewMeal = async () => {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const data = await response.json();
     if (data && data.meals && data.meals.length > 0) {
       const newMeal = data.meals[0];
       setMeal(newMeal);
       storeMeal(newMeal);
     }
-  }, [data]);
-
-  if (error) {
-    return (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="text-2xl font-bold">Failed to Load</div>
-      </div>
-    );
-  }
+  };
 
   if (!meal) {
     return (
@@ -47,33 +37,37 @@ const MealOfTheDay = () => {
   }
 
   return (
-    <div className="font-sans p-6  mx-auto flex flex-row items-center ">
-      <div>
-        <h1 className="text-3xl font-bold mb-4 text-center p-2">
-          Meal of the Day
-        </h1>
-      </div>
-      <div>
-        <Link to={`/meal/${meal.idMeal}`} key={meal.idMeal} className="w-full">
-          <div className="relative ">
-            <img
-              src={meal.strMealThumb}
-              alt={meal.strMeal}
-              className="w-full h-full object-cover rounded-xl shadow-lg mb-4 "
-            />
-            <div className="absolute bottom-4 left-4 bg-white bg-opacity-45 p-4 rounded-lg">
-              <h2 className="text-2xl font-bold">{meal.strMeal}</h2>
-              <p className="text-ls text-gray-700">
-                {meal.strCategory} | {meal.strArea}
-              </p>
+    <div className="min-h-[70vh] flex items-start  mt-[200px] justify-center ">
+      <div className="font-sans p-6 mx-auto flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-8 max-w-6xl">
+        <div className="w-full md:w-1/4 text-center">
+          <h1 className="text-5xl font-bold mb-4">Meal of the Day</h1>
+        </div>
+        <div className="w-full md:w-1/2 flex justify-center">
+          <Link
+            to={`/meal/${meal.idMeal}`}
+            key={meal.idMeal}
+            className="w-full"
+          >
+            <div className="relative">
+              <img
+                src={meal.strMealThumb}
+                alt={meal.strMeal}
+                className="w-full h-80 object-cover rounded-xl shadow-lg mb-4"
+              />
+              <div className="absolute bottom-4 left-4 bg-white bg-opacity-45 p-4 rounded-lg">
+                <h2 className="text-2xl font-bold">{meal.strMeal}</h2>
+                <p className="text-lg text-gray-700">
+                  {meal.strCategory} | {meal.strArea}
+                </p>
+              </div>
             </div>
-          </div>
-        </Link>
-      </div>
-      <div>
-        <p className="mt-4 text-gray-600 text-center text-lg mb-3">
-          {meal.strInstructions.substring(0, 150)}...
-        </p>
+          </Link>
+        </div>
+        <div className="w-full md:w-1/4">
+          <p className="mt-4 text-gray-600 text-center text-lg mb-3">
+            {meal.strInstructions.substring(0, 150)}...
+          </p>
+        </div>
       </div>
     </div>
   );
