b209db8b80f44730acad351912112dabdfa68c76
seolyam <leeyam.dev@gmail.com>
2024-07-29 10:52:51 +0800

    chore: update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,10 @@ export default function About() {
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-2xl font-bold mb-4">About</h2>
-      <p>This is a simple meal app using the Themealdb API.</p>
+      <p>
+        This is a simple meal app using the Themealdb API. My 2nd project after
+        the RoutineApp. let me cook!
+      </p>
     </div>
   );
 }
