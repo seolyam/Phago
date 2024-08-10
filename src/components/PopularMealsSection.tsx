b6e466df595ec98eb6201f2f671bf2f5725b83dd
seolyam <leeyam.dev@gmail.com>
2024-08-10 10:31:16 +0800

    feat: use carousel for popular meals

--- src/components/PopularMealsSection.tsx
+++ src/components/PopularMealsSection.tsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom";
 import useSWR from "swr";
+import { Card, CardContent } from "../components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../components/ui/carousel";
 
 interface Meal {
   idMeal: string;
@@ -23,25 +31,40 @@ const PopularMealsSection = () => {
   const meals: Meal[] = data.meals.slice(0, 6);
 
   return (
-    <section className="py-4 bg-white ">
+    <section className="py-4 bg-white">
       <div className="max-w-6xl mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8 text-center">Popular Meals</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {meals.map((meal) => (
-            <div className="text-center" key={meal.idMeal}>
-              <img
-                src={meal.strMealThumb}
-                alt={meal.strMeal}
-                className="rounded-lg mb-4 transition-transform duration-200 ease-in-out transform hover:scale-105"
-              />
-              <h3 className="text-xl font-semibold mb-2">{meal.strMeal}</h3>
-              <p className="mb-4">{meal.strCategory}</p>
-              <Link to={`/meal/${meal.idMeal}`} className="">
-                Learn more
-              </Link>
-            </div>
-          ))}
-        </div>
+        <Carousel opts={{ align: "start" }} className="w-full max-w-6xl">
+          <CarouselContent>
+            {meals.map((meal) => (
+              <CarouselItem key={meal.idMeal} className="md:basis-1/3">
+                <div className="p-2">
+                  <Card>
+                    <CardContent className="flex flex-col items-center justify-center p-4">
+                      <img
+                        src={meal.strMealThumb}
+                        alt={meal.strMeal}
+                        className="rounded-lg mb-4 transition-transform duration-200 ease-in-out transform hover:scale-105"
+                      />
+                      <h3 className="text-xl font-semibold mb-2">
+                        {meal.strMeal}
+                      </h3>
+                      <p className="mb-4">{meal.strCategory}</p>
+                      <Link
+                        to={`/meal/${meal.idMeal}`}
+                        className="text-blue-500"
+                      >
+                        Learn more
+                      </Link>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </section>
   );
